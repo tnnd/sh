62b16c2b9be9df4f6056ef32ee9d0db3b807fe8c
tnnd <37441897+tnnd@users.noreply.github.com>
2021-09-01 15:16:20 +0800

    Update linux1.sh

--- other/linux1.sh
+++ other/linux1.sh
@@ -1,5 +1,4 @@
 ## debian
 docker run --privileged --shm-size=1g --name ibm -d -it -p 33890:3389 -p 20080:8080 -p 29022:22 s390x/debian:buster
 docker exec -it ibm /bin/bash
-apt update && apt -y install dialog xfce4 xrdp nano midori fonts-wqy-microhei terminator
-service xrdp start
+apt update && apt -y install dialog xfce4 xrdp nano midori fonts-wqy-microhei terminator && service xrdp start
