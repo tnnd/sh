282b4bb6cf6d47625920d13e71f1d8985581dc72
tnnd <37441897+tnnd@users.noreply.github.com>
2022-01-26 17:30:21 +0800

    hdhome签到
    需要添加cookies

---
+++ other/hdhome.sh
@@ -0,0 +1,7 @@
+curl -o /dev/null  'https://hdhome.org/attendance.php' \
+	-H 'Connection: keep-alive'-H 'Upgrade-Insecure-Requests: 1' \
+	-H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux aarch64; rv:91.0) Gecko/20100101 Firefox/91.0' \
+	-H 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+	-H 'Referer: https://hdhome.org/mybonus.php' \
+	-H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' \
+  -H 'cookies:'
